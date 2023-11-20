1c553c6983e913a0e618cd1d64f0c5bdcb1c0648
Junghwan <sl8792@naver.com>
2023-11-20 11:27:37 +0900

    Chapter03 연산자 형변환

---
+++ src/chapter03/OperatorEx6.java
@@ -0,0 +1,10 @@
+package chapter03;
+
+public class OperatorEx6 {
+    static public void main(String args[]) {
+        byte a = 10;
+        byte b = 20;
+        byte c = (byte)(a + b);
+        System.out.println(c);
+    }
+}
