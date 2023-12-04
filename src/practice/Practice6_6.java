009ce8f67bbaf2758a5b9cd79ec776f45f2689b8
Junghwan <sl8792@naver.com>
2023-12-04 09:23:47 +0900

    chore: 오타 수정

--- src/practice/Practice6_6.java
+++ src/practice/Practice6_6.java
@@ -1,6 +1,6 @@
 package practice;
 
-public class Practicce6_6 {
+public class Practice6_6 {
 
     static double getDistance(int x, int y, int x1, int y1) {
         return Math.sqrt(((x1-x)*(x1-x)) + ((y1-y)*(y1-y)));
